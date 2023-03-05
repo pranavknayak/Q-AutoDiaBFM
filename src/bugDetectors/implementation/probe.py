be4f7c0480e9e0ae4d1bcbee80f1e5270d4a7e31
KrishnKher <zetakrh@gmail.com>
2023-03-05 22:16:54 +0530

    Refined some probe.py files'

--- src/bugDetectors/implementation/probe.py
+++ src/bugDetectors/implementation/probe.py
@@ -1,3 +1,10 @@
 import sys
 import threading
 from ..probe import threadingStyle # verify if it really works
+
+def assessBugType(editScript, style: threadingStyle):
+    prune = False
+    bugType = "random"
+
+
+    return prune, bugType
