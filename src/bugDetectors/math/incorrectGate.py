6f2d6394d405686451d6d52e1bba766d5aea0a83
KrishnKher <zetakrh@gmail.com>
2023-03-06 17:29:27 +0530

    Minor filename changes and some code in IncorrectGate.py

--- src/bugDetectors/math/incorrectGate.py
+++ src/bugDetectors/math/incorrectGate.py
@@ -2,3 +2,11 @@
 # note that there are only a finite number of inbuilt gates in qiskit
 # provided the identifier is the same, check if the other files' gate matches with an inbuilt gate.
 # incase it is a custom made gate, think
+import ast
+
+
+def detectIncorrectGate(edit_script):
+    prune = True # We have to prune it anyway but it is not the same as status, the actual thing
+
+    return 
+
