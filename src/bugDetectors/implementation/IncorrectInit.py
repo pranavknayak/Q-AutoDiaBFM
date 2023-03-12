636e165364a4efeffa2fe7751fcd7a4226b781d8
123Ishan <32106139+123Ishan@users.noreply.github.com>
2023-03-12 10:59:27 -0400

    Update IncorrectInit.py

--- src/bugDetectors/implementation/IncorrectInit.py
+++ src/bugDetectors/implementation/IncorrectInit.py
@@ -5,8 +5,27 @@
 import ast
 
 
-def detectIncorrectInit(codeSample):
+def detectIncorrectInit(editScript):
     status = False
     bugTypeMessage = "Incorrect initialization(s) attempted."
+
+    # Identify variables being initialized in the script
+    variables = []
+    for line in editScript.split('\n'):
+        if '=' in line:
+            variable_name = line.split('=')[0].strip()
+            variables.append(variable_name)
+
+    # Check each variable initialization for correctness
+    for variable in variables:
+        try:
+            exec(editScript)
+            exec(f"type({variable})") # Check the data type of the variable
+        except:
+            # If the variable is not initialized correctly, set status and update bugTypeMessage
+            status = True
+            bugTypeMessage = f"Incorrect initialization(s) attempted for variable {variable}."
+            break
+    
 
     return status, bugTypeMessage
