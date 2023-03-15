9e5bcea73cd4ea4a608f8c25126d521dfa559284
KrishnKher <zetakrh@gmail.com>
2023-03-15 16:43:09 +0530

    Cleaned up IncorrectGate a bit

--- src/bugDetectors/math/IncorrectGate.py
+++ src/bugDetectors/math/IncorrectGate.py
@@ -10,11 +10,6 @@ def inbuiltGateError(codeSample):
     availableInbuiltGates = ['ccx', 'cx', 'h', 'i', 'p', 's', 'sdg', 't', 'tdg', 'u', 'x', 'y', 'z']
     regexPattern = " *Update\(\(identifier:.+, *line [0-9]+:[0-9] - [0-9]+:[0-9]\), .+\) *"
     buggy, patched = codeSample[0], codeSample[2]
-    buggyList, patchedList = buggy.split('\n'), patched.split('\n')
-    modify = ""
-    for j in buggyList:
-        modify += j + "\n"
-    print(modify)
     buggyID, patchedID = {}, {}
 
     astBuggy, astPatched = ast.walk(ast.parse(buggy)), ast.walk(ast.parse(patched))
@@ -33,9 +28,7 @@ def inbuiltGateError(codeSample):
     ''' Considering the cases when there is a one to one mapping of the QuantumCircuits 
     in buggy code to the QuantumCircuits in patched code. '''
 
-    #print(codeSample[1])
-
-    if len(buggyID) == 0 or len(buggyID) != len(patchedID):
+    if len(buggyID) != len(patchedID):
         return False
 
     if len(codeSample[1]) == 1:
@@ -45,28 +38,11 @@ def inbuiltGateError(codeSample):
             buggyGate = editScriptStringed.split("((identifier:")[1].split(",")[0]
             patchedGate = editScriptStringed.split("), ")[1].split(")")[0]
             if (buggyGate not in availableInbuiltGates) or (patchedGate not in availableInbuiltGates) or (buggyGate == patchedGate):
-                return False
-            
-                 
+                return False     
         else:
             return False
     else:
         return False
-
-    # if len(codeSample[1]) == 1:
-    #     editScriptStringed = str(codeSample[1])[2:-2]
-    #     temporaryStatus = re.search(regexPattern, editScriptStringed)
-    #     if temporaryStatus is not None:
-    #         buggyGate = editScriptStringed.split("((identifier:")[1].split(",")[0]
-    #         patchedGate = editScriptStringed.split("), ")[1].split(")")[0]
-    #         if (buggyGate not in availableInbuiltGates) or (patchedGate not in availableInbuiltGates) or (buggyGate == patchedGate):
-    #             return False
-            
-            
-    #     else:
-    #         return False
-    # else:
-    #     return False
 
     return True
 
