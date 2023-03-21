6498f3f02bc5056528b48da91e56f398c9190410
KrishnKher <zetakrh@gmail.com>
2023-03-21 21:16:33 +0530

    Made a minor update to IncorrectGate

--- src/bugDetectors/unitary/IncorrectGate.py
+++ src/bugDetectors/unitary/IncorrectGate.py
@@ -1,3 +1,7 @@
+# compare the diff lines
+# note that there are only a finite number of inbuilt gates in qiskit
+# provided the identifier is the same, check if the other files' gate matches with an inbuilt gate.
+# incase it is a custom made gate, think
 import ast
 import re
 
@@ -58,6 +62,12 @@ def inbuiltGateError(codeSample):
 def detectIncorrectGate(codeSample):
     status = False
     bugTypeMessage = "Incorrect usage of gate(s)."
+    '''
+    1. Inbuilt gates based errors.
+        a. single line errors.
+        b. multiline errors.
+    2. Customised gates based errors.
+    '''
     status |= inbuiltGateError(codeSample)
 
     return status, bugTypeMessage
