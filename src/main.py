78339056ace919602cbc4843660275ebad028ed8
Pranav K Nayak <es20btech11035@iith.ac.in>
2023-09-08 14:53:12 +0530

    report all bugs

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
-#TODO: Report all bugs
-#TODO: Refactor to reduce number of AST passes
 #TODO: Tests
-"""This code imports a module named "bugInvestigator" with an alias "bI". 
+"""This code imports a module named "bugInvestigator" with an alias "bI".
 It then calls the function "classifyBugs" from the module with three arguments and prints the returned message to the console.
 """
 import bugInvestigator as bI
 
-bugErrorMessage = bI.classifyBugs(buggy="test0", patched="test1", commandLine=False)
+
+buggyCode = '''
+creg = ClassicalRegister(5)
+qreg = QuantumRegister(6)
+qc = QuantumCircuit(creg, qreg)
+'''
+
+patchedCode = '''
+creg1 = ClassicalRegister(5)
+qreg = QuantumRegister(5)
+qc = QuantumCircuit(creg1, qreg)
+'''
+
+# Should return incorrectGate and incorrectInit
+
+bugErrorMessage = bI.classifyBugs(buggy=buggyCode, patched=patchedCode, commandLine=False)
 print(bugErrorMessage)
